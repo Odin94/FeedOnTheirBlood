6a301452e129368a3491c48136ef253a304b89ad
Odin Kammerloher <kammerloher.andreas@gmail.com>
2022-12-14 18:49:20 +0100

    Add direct logout to navbar

--- frontend/src/components/AppShellNavbar.tsx
+++ frontend/src/components/AppShellNavbar.tsx
@@ -1,7 +1,7 @@
 import { Box, Center, Navbar, Stack, Text } from "@mantine/core"
 import { Link } from "react-router-dom"
 import { useMyClan } from "../api/clans.types";
-import { useSession } from "../api/user.type"
+import { useSession, useSignOut } from "../api/user.type"
 
 import moonIcon from '../images/full-moon-halloween-svgrepo-com.svg';
 import knifeIcon from '../images/knife-svgrepo-com.svg';
@@ -11,12 +11,13 @@ import crownIcon from '../images/crown-svgrepo-com.svg'
 import policeIcon from '../images/police-badge-svgrepo-com.svg'
 
 
-{/* filter property for coloring svgs: https://codepen.io/sosuke/pen/Pjoqqp */ }
+/* filter property for coloring svgs: https://codepen.io/sosuke/pen/Pjoqqp */
 const oliveSvgFilter = "invert(96%) sepia(4%) saturate(1720%) hue-rotate(217deg) brightness(111%) contrast(100%)"
 
 const AppShellNavbar = () => {
     const { data: session } = useSession()
     const { data: clan } = useMyClan()
+    const signOutMutation = useSignOut()
 
     return (
         <Navbar width={{ base: 300 }} p="xs" >
@@ -30,29 +31,40 @@ const AppShellNavbar = () => {
                 {
                     < div style={{ display: "flex", flexDirection: "column", alignItems: "stretch", height: "100%" }}>
                         <Center style={{ height: "100%" }}>
-                            <Box style={{ textAlign: "left", maxWidth: "250px" }} mx="auto">
+                            <Box style={{ textAlign: "left", width: "250px" }} mx="auto">
                                 {!session
-                                    ? <Text component={Link} variant="link" to="/signup">
-                                        <img alt="knife" src={knifeIcon} width="40" style={{ filter: oliveSvgFilter, marginRight: "10px" }} /> Signup
-                                    </Text>
-                                    : null
+                                    ? <>
+                                        <Text component={Link} variant="link" to="/signup">
+                                            <img alt="knife" src={knifeIcon} width="40" style={{ filter: oliveSvgFilter, marginRight: "10px" }} /> Signup
+                                        </Text>
+                                        <br />
+                                        <Text component={Link} variant="link" to="/login">
+                                            <img alt="knife" src={knifeIcon} width="40" style={{ filter: oliveSvgFilter, marginRight: "10px" }} /> Login
+                                        </Text>
+                                    </>
+                                    : <>
+                                        <br />
+                                        <Text component={Link} variant="link" onClick={() => signOutMutation.mutate()} to="/login" >
+                                            <img alt="knife" src={knifeIcon} width="40" style={{ filter: oliveSvgFilter, marginRight: "10px" }} /> Logout
+                                        </Text>
+                                        <br />
+                                        <Text component={Link} variant="link" to="/">
+                                            <img alt="knife" src={knifeIcon} width="40" style={{ filter: oliveSvgFilter, marginRight: "10px" }} /> Vampires
+                                        </Text>
+                                        <br />
+                                        <Text component={Link} variant="link" to="/create">
+                                            <img alt="knife" src={knifeIcon} width="40" style={{ filter: oliveSvgFilter, marginRight: "10px" }} /> Create New Vampire
+                                        </Text>
+                                        <br />
+                                        <Text component={Link} variant="link" to="/hunt">
+                                            <img alt="knife" src={knifeIcon} width="40" style={{ filter: oliveSvgFilter, marginRight: "10px" }} /> Hunt
+                                        </Text>
+                                        <br />
+                                        <Text component={Link} variant="link" to="/lair">
+                                            <img alt="knife" src={knifeIcon} width="40" style={{ filter: oliveSvgFilter, marginRight: "10px" }} /> Lair
+                                        </Text>
+                                    </>
                                 }
-                                <br />
-                                <Text component={Link} variant="link" to="/login">
-                                    <img alt="knife" src={knifeIcon} width="40" style={{ filter: oliveSvgFilter, marginRight: "10px" }} /> Login
-                                </Text>
-                                <br />
-                                <Text component={Link} variant="link" to="/">
-                                    <img alt="knife" src={knifeIcon} width="40" style={{ filter: oliveSvgFilter, marginRight: "10px" }} /> Vampires
-                                </Text>
-                                <br />
-                                <Text component={Link} variant="link" to="/create">
-                                    <img alt="knife" src={knifeIcon} width="40" style={{ filter: oliveSvgFilter, marginRight: "10px" }} /> Create New Vampire
-                                </Text>
-                                <br />
-                                <Text component={Link} variant="link" to="/hunt">
-                                    <img alt="knife" src={knifeIcon} width="40" style={{ filter: oliveSvgFilter, marginRight: "10px" }} /> Hunt
-                                </Text>
                             </Box>
                         </Center>
                     </div >
