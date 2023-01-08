7b252e01c8d8dfb21f243d6fb1f0a693f12f59c8
Odin Kammerloher <kammerloher.andreas@gmail.com>
2023-01-08 11:27:44 +0100

    Add a healing button that lets vampires trade blood for health

--- frontend/src/components/vampire/VampireCard.tsx
+++ frontend/src/components/vampire/VampireCard.tsx
@@ -1,10 +1,28 @@
-import { Badge, Card, Group, Image, Text } from '@mantine/core';
+import { Badge, Button, Card, Grid, Group, Image, Text } from '@mantine/core';
+import { useState } from 'react';
+import { useQueryClient } from 'react-query';
 
-import { Vampire } from "../../api/vampires.type";
+import { useUpdateVampire, Vampire, vampiresKey } from "../../api/vampires.type";
 import bloodIcon from '../../images/blood-svgrepo-com.svg';
 import healthIcon from '../../images/health-cross-svgrepo-com.svg';
 
 const VampireCard = ({ vampire, children }: { vampire: Vampire, children?: React.ReactNode }) => {
+    const queryClient = useQueryClient()
+    const [healing, setHealing] = useState(false)
+    const vampireMutation = useUpdateVampire()
+
+    const healVampire = () => {
+        if (vampire.current_health >= vampire.max_health || vampire.current_blood <= 0) return
+        setHealing(true)
+        vampireMutation.mutate({
+            ...vampire,
+            current_health: vampire.current_health += 1,
+            current_blood: vampire.current_blood -= 1
+        }, {
+            onSuccess: () => { queryClient.invalidateQueries(vampiresKey); setHealing(false) }
+        })
+    }
+
     return (
         <Card shadow="sm" p="lg" radius="md" withBorder>
             <Card.Section>
@@ -25,10 +43,19 @@ const VampireCard = ({ vampire, children }: { vampire: Vampire, children?: React
             <Text size="sm" color="dimmed">
                 This is a real cool vampire sample text lorem ipsum dolor sit amet
             </Text>
-            <Text size="sm" color="dimmed">
-                {/* filter property for coloring svgs: https://codepen.io/sosuke/pen/Pjoqqp */}
-                <img alt="blood" src={healthIcon} width="20" style={{ filter: "invert(96%) sepia(4%) saturate(1720%) hue-rotate(217deg) brightness(111%) contrast(100%)" }} /> {vampire?.current_health} / {vampire?.max_health}
-            </Text>
+
+            <Grid>
+                <Grid.Col span={3}>
+                    <Text size="sm" color="dimmed">
+                        {/* filter property for coloring svgs: https://codepen.io/sosuke/pen/Pjoqqp */}
+                        <img alt="blood" src={healthIcon} width="20" style={{ filter: "invert(96%) sepia(4%) saturate(1720%) hue-rotate(217deg) brightness(111%) contrast(100%)" }} /> {vampire?.current_health} / {vampire?.max_health}
+                    </Text>
+                </Grid.Col>
+                <Grid.Col offset={7} span={2}>
+                    <Button compact onClick={() => { healVampire() }}
+                        color="red.0" disabled={vampire.current_blood <= 0 || vampire.current_health === vampire.max_health || healing}><Text color={"black"}>Heal</Text></Button>
+                </Grid.Col>
+            </Grid>
             <Text size="sm" color="dimmed">
                 <img alt="blood" src={bloodIcon} width="20" /> {vampire?.current_blood} / {vampire?.max_blood}
             </Text>
